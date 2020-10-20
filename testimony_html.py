148b222a3b333e150994583befc2e1a5ae2de3c2
Christine Liu <clue816@gmail.com>
2020-10-19 18:14:20 -0700

    Read all directory files, prepped for export into csv

--- testimony_html.py
+++ testimony_html.py
@@ -6,6 +6,6 @@ else:
      from io import BytesIO as StringIO
 from pdfminer.layout import LAParams
 output_string = StringIO()
-with open('Abraham Peter_37735_M.pdf', 'rb') as fin:
+with open('../../Hunger/Lodz Transcripts/Sam Fasenfast_9728_M.pdf', 'rb') as fin:
     extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='html', codec=None)
     print(output_string.getvalue())
--- testimony_parser.py
+++ testimony_parser.py
@@ -1,26 +1,48 @@
+import os
+import glob
+
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar, LTTextBoxHorizontal
 
-def hasNumbers(inputString):
-    return any(char.isdigit() for char in inputString)
+def has_numbers(input_string):
+    return any(char.isdigit() for char in input_string)
 
 record = False
+survivor_name = ""
+path = "../../Hunger/Lodz Transcripts"
 
-for page in extract_pages("./Abraham Peter_37735_M.pdf"):
-    for element in page:
-        if isinstance(element, LTTextBoxHorizontal):
-            if element.get_text() == "Transcript\n":
-                record = False
-                
-            if record == True:
-                if hasNumbers(element.get_text()): 
-                    continue
-                if "VHA" in element.get_text():
-                    continue
-                else: 
-                    print(element.get_text())
-            
-                
-            if element.get_text() == "All People\n": 
-                record = True
-            
+for file_name in glob.glob(os.path.join(path, "*.pdf")):
+    for page in extract_pages(file_name):
+        for element in page:
+            if isinstance(element, LTTextBoxHorizontal):
+                if "VHA - Testimony" in element.get_text():
+                    survivor_name = element.get_text().split(" - ")[2]
+
+                if element.get_text() == "Transcript\n":
+                    record = False
+
+                if record == True:
+                    name_and_description = element.get_text()
+                    if has_numbers(name_and_description): 
+                        continue
+
+                    elif "VHA" in name_and_description:
+                        continue
+
+                    else:
+                        name = name_and_description.split(" - ")[0]
+                        first_name = name.split(" ")[0]
+                        if " " in name: 
+                            last_name = name.split(" ")[1]
+                        else:
+                            last_name = ""
+                        try:
+                            description = name_and_description.split(" - ")[1].rstrip("\n")
+                        except IndexError:
+                            continue
+
+                        print(name + ", " + first_name + ", " + last_name + ", " + "[" + description + "]" + ", " + survivor_name)
+
+                if element.get_text() == "All People\n": 
+                    record = True
+
