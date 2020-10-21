91bf6f4567c0bb828a57bc4dbb3aae66b5585f36
Christine Liu <clue816@gmail.com>
2020-10-21 00:17:08 -0700

    Export to csv and log repeat file names

--- testimony_parser.py
+++ testimony_parser.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import csv
 
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar, LTTextBoxHorizontal
@@ -10,39 +11,49 @@ def has_numbers(input_string):
 record = False
 survivor_name = ""
 path = "../../Hunger/Lodz Transcripts"
+file_names_with_errors = []
 
-for file_name in glob.glob(os.path.join(path, "*.pdf")):
-    for page in extract_pages(file_name):
-        for element in page:
-            if isinstance(element, LTTextBoxHorizontal):
-                if "VHA - Testimony" in element.get_text():
-                    survivor_name = element.get_text().split(" - ")[2]
+with open("Lodz_Transcript_Names.csv", mode="w") as names_file:
+    file_writer = csv.writer(names_file, delimiter=",")
+    for file_name in glob.glob(os.path.join(path, "*.pdf")):
+        for page in extract_pages(file_name):
+            for element in page:
+                if isinstance(element, LTTextBoxHorizontal):
+                    if "VHA - Testimony" in element.get_text():
+                        survivor_name = element.get_text().split(" - ")[2].rstrip("\n")
 
-                if element.get_text() == "Transcript\n":
-                    record = False
+                    if element.get_text() == "Transcript\n":
+                        record = False
 
-                if record == True:
-                    name_and_description = element.get_text()
-                    if has_numbers(name_and_description): 
-                        continue
-
-                    elif "VHA" in name_and_description:
-                        continue
-
-                    else:
-                        name = name_and_description.split(" - ")[0]
-                        first_name = name.split(" ")[0]
-                        if " " in name: 
-                            last_name = name.split(" ")[1]
-                        else:
-                            last_name = ""
-                        try:
-                            description = name_and_description.split(" - ")[1].rstrip("\n")
-                        except IndexError:
+                    if record == True:
+                        name_and_description = element.get_text()
+                        if has_numbers(name_and_description): 
                             continue
 
-                        print(name + ", " + first_name + ", " + last_name + ", " + "[" + description + "]" + ", " + survivor_name)
+                        elif "VHA" in name_and_description:
+                            continue
 
-                if element.get_text() == "All People\n": 
-                    record = True
+                        else:
+                            name = name_and_description.split(" - ")[0]
+                            first_name = name.split(" ")[0]
+                            
+                            if " " in name: 
+                                last_name = name.split(" ")[1]
+                            else:
+                                last_name = ""
+                                
+                            try:
+                                description = name_and_description.split(" - ")[1].rstrip("\n")
+                            except IndexError:
+                                file_names_with_errors.append(file_name)
+                                continue
+
+#                            print(name + ", " + first_name + ", " + last_name + ", " + "[" + description + "]" + ", " + survivor_name)
+                            
+                            file_writer.writerow([name, first_name, last_name, description, survivor_name])
+                            
+                    if element.get_text() == "All People\n": 
+                        record = True
+
+print(file_names_with_errors)
 
